cf39d954ac51def168be5fca177844d8fc96653b
hatem.hadrich <hatem.hadrich@sifast.com>
2017-11-06 10:40:00 +0100

    Fixing description text

--- test/campaigns/googleAdwords/1-CheckConfiguration.js
+++ test/campaigns/googleAdwords/1-CheckConfiguration.js
@@ -1,19 +1,19 @@
-scenario('Check the configuration of "Google adwords" in the Back Office', client => {
+scenario('Check the voucher code of "Google adwords" module', client => {
     scenario('Login in Back Office', client => {
         test('should open the browser', () => client.open());
         test('should login successfully in BO', () => client.signInBO());
     }, "gadwords-client");
-    scenario('Check the configuration in Back Office', client => {
+    scenario('Check the voucher code in "Google Adwords" module configuration page', client => {
         test('should go to "Installed modules" page', () => client.goToModulesPage());
         test('should search "gadwords" module', () => client.searchModule('gadwords'));
         test('should check "gadwords" module', () => client.getInstalledModulesNumber());
         test('should check "Configure" button', () => client.getModuleButtonName());
         test('should click on "Configure" button', () => client.clickConfigureModuleButton('gadwords'));
         test('should check "Google AdWords" promotional code', () => client.checkGoogleAdwordsPromotionalCode());
-        test('should click on "start your campaign now with your promotional code" button', () => client.clickOnStartButton());
-        test('should check "Google Adwords" voucher is shown', () => client.checkGoogleAdwordsVoucher());
+        test('should click on "Start your campaign now with your promotional code" button', () => client.clickOnStartButton());
+        test('should check "Google Adwords" voucher', () => client.checkGoogleAdwordsVoucher());
     }, "gadwords-client");
-    scenario('Log out from the Back Office', client => {
-        test('should log out successfully from the Back Office', () => client.signOutBO());
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from the Back Office', () => client.signOutBO());
     }, "gadwords-client");
 }, "gadwords-client", true);

7b8f2da54dfe762eb978d154ea7fae5d18963a19
hatem.hadrich <hatem.hadrich@sifast.com>
2017-11-06 09:52:25 +0100

    Add some fixes to the script

--- test/campaigns/googleAdwords/1-CheckConfiguration.js
+++ test/campaigns/googleAdwords/1-CheckConfiguration.js
@@ -1,18 +1,19 @@
-// @TODO: fix connecting with google account in FO when we buy product(error 502 bad gatway)
-scenario('Test google adwords', client => {
-    scenario('Log in in Back Office', client => {
+scenario('Check the configuration of "Google adwords" in the Back Office', client => {
+    scenario('Login in Back Office', client => {
         test('should open the browser', () => client.open());
-        test('should log in successfully in BO', () => client.fillSignInForm());
-    }, "googleAdwords/gadwords-client");
+        test('should login successfully in BO', () => client.signInBO());
+    }, "gadwords-client");
     scenario('Check the configuration in Back Office', client => {
-        test('should acces to module page', () => client.goToModule());
-        test('should search the module', () => client.searchModule('gadwords'));
-        test('should click on configure button', () => client.clickOnConfigureButton());
-        test('should check google adWords promotional code', () => client.checkGoogleAdwordsPromotionalCode());
-        test('should click on start your campaign now with your promotional code button', () => client.clickOnStartButton());
-        test('should check google adwords voucher is shown', () => client.checkGoogleAdwordsVoucher());
-    }, "googleAdwords/gadwords-client");
+        test('should go to "Installed modules" page', () => client.goToModulesPage());
+        test('should search "gadwords" module', () => client.searchModule('gadwords'));
+        test('should check "gadwords" module', () => client.getInstalledModulesNumber());
+        test('should check "Configure" button', () => client.getModuleButtonName());
+        test('should click on "Configure" button', () => client.clickConfigureModuleButton('gadwords'));
+        test('should check "Google AdWords" promotional code', () => client.checkGoogleAdwordsPromotionalCode());
+        test('should click on "start your campaign now with your promotional code" button', () => client.clickOnStartButton());
+        test('should check "Google Adwords" voucher is shown', () => client.checkGoogleAdwordsVoucher());
+    }, "gadwords-client");
     scenario('Log out from the Back Office', client => {
         test('should log out successfully from the Back Office', () => client.signOutBO());
-    }, "googleAdwords/gadwords-client");
-}, "googleAdwords/gadwords-client", true);
+    }, "gadwords-client");
+}, "gadwords-client", true);
--- test/clients/gadwords-client.js
+++ test/clients/gadwords-client.js
@@ -1,47 +1,7 @@
-const {getClient} = require('../../common.webdriverio');
-const {selector} = require('../../globals.webdriverio.js');
+const CommonClient = require('./common_client');
+var {selector} = require('../globals.webdriverio.js');
 
-class GoogleAdwords {
-    constructor() {
-        this.client = getClient();
-    }
-
-    fillSignInForm() {
-        return this.client.signinBO();
-    }
-
-    signOutBO() {
-        return this.client.signoutBO();
-    }
-    close() {
-        return this.client.end();
-    };
-
-    open() {
-        return this.client.init().windowHandleSize({width: 1280, height: 1024});
-    }
-
-    goToModule() {
-        return this.client
-            .click(selector.BO.ModulesPage.modules_subtab)
-            .waitForExist(selector.BO.ModulesPage.page_loaded, 60000)
-            .pause(5000);
-    }
-
-    clickOnConfigureButton() {
-        return this.client
-            .waitForExist(selector.BO.ModulesPage.configuration_button, 90000)
-            .click(selector.BO.ModulesPage.configuration_button)
-            .pause(5000);
-    }
-
-    searchModule(name) {
-        return this.client
-            .waitForExist(selector.BO.ModulesPage.search_input, 60000)
-            .setValue(selector.BO.ModulesPage.search_input, name)
-            .click(selector.BO.ModulesPage.search_button)
-            .pause(5000);
-    }
+class GoogleAdwords extends CommonClient{
 
     checkGoogleAdwordsPromotionalCode(){
         return this.client
@@ -77,10 +37,6 @@ class GoogleAdwords {
             .then(() => this.client.getTabIds())
             .then(ids => this.client.switchTab(ids[0]))
             .pause(5000);
-    }
-
-    takeScreenshot() {
-        return this.client.saveScreenshot(`test/screenshots/${this.client.desiredCapabilities.browserName}_exception_${global.date_time}.png`);
     }
 }
 
